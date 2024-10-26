9c9ada283ef9b255adea0034634643dc7491af9d
Andrei Malets <coolviktori@yandex.by>
2024-10-26 22:54:26 +0300

    changes End

--- src/components/EndInfo.tsx
+++ src/components/EndInfo.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useState } from "react";
 import goodImage from "../assets/images/goodJob.png";
+import starIcon from "../assets/images/star.png";
 
 type EndInfoProps = {
   resetGame: () => void;
@@ -16,9 +17,12 @@ const EndInfo = ({ resetGame }: EndInfoProps) => {
         <>
           <GoodJobImg />
           <StarsPointsContainer>
-            <NotGood />
-            <Normal />
-            <NiceJob />
+            <NotGood>
+              {1}
+              <StarItem></StarItem>
+            </NotGood>
+            <Normal>{3}</Normal>
+            <NiceJob>{5}</NiceJob>
           </StarsPointsContainer>
           <EndGameButtonContainer>
             <EndGameButtonYes onClick={resetGame}>Return</EndGameButtonYes>
@@ -51,19 +55,12 @@ const StarsPointsContainer = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  // height: 55px;
-  // width: 400px;
-  // padding: 15px 25px;
-  // font-size: 24px;
-  // cursor: pointer;
-  // outline: none;
-  // color: #fff;
-  // background: #8bdc00;
-  // border: none;
-  // border-radius: 15px;
-  // box-shadow: 0 5px #7ac70c;
 `;
 const NotGood = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   width: 500px;
   height: 55px;
   margin: auto;
@@ -71,16 +68,32 @@ const NotGood = styled.div`
   border: 1px solid red;
 `;
 const Normal = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   width: 500px;
   height: 55px;
   background: #fff;
   border: 1px solid red;
 `;
 const NiceJob = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   width: 500px;
   height: 55px;
   background: #fff;
   border: 1px solid red;
+`;
+const StarItem = styled.div`
+  width: 50px;
+  height: 50px;
+  background: url(${starIcon});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
 `;
 const EndGameButtonYes = styled.button`
   display: flex;
